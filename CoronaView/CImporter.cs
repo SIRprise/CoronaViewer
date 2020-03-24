266331bd51fda9e70cca2a658a48968cf61b3491
SIRprise <peter3@turboprinz.de>
2020-03-24 07:45:47 +0100

    updatet URL (working ok, again)

--- CoronaView/CImporter.cs
+++ CoronaView/CImporter.cs
@@ -18,7 +18,7 @@ namespace CoronaView
         {
             Dictionary<string, CCountryDataset> resultDict = new Dictionary<string, CCountryDataset>();
 
-            string sourceURL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv";
+            string sourceURL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
             string sourceData = GetStringDataFromURL(sourceURL);
 
